7cde04ab13388a3e2fc387b05e1dbe84c9fcc136
Keerthivasan <pkvasan99@outlook.com>
2023-03-25 04:14:44 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_d5eGSayyy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3f1h1g5ftk49isip5r9u5rlic4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_rBr7GFtr0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4du6gj9qg2lgm1dqk40jkmf7lv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
